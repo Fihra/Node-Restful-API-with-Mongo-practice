6e92dbf2a28ba5d0e894e11f82f77dd6fa9a49f7
Fabian Fabro <fabrofabian@gmail.com>
2019-12-04 15:41:31 -0800

    connected to mongodb

--- app.js
+++ app.js
@@ -1,3 +1,28 @@
 const express = require('express');
+const app = express();
+const mongoose = require('mongoose');
 
-const app =express();
+
+//Middlewares
+// app.use('/posts', () => {
+//     console.log('This is a middleware running');
+// })
+
+//ROUTES
+app.get('/', (req, res) => {
+    res.send("Hello World");
+})
+
+app.get('/posts', (req, res) => {
+    res.send("We are on posts");
+})
+
+//Connect to DB
+mongoose.connect('localhost:27017/UserDB', {useNewUrlParser: true}, () => {
+    console.log("Connected to DB!");
+});
+
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+    console.log(`Listening to ${port}`);
+});
